45270c2c21ae785442dc7765f314ea55a46ce7ac
Guilherme Oliveira <dev.guilherme7@gmail.com>
2025-04-03 01:20:07 -0300

    Add basic security configuration

--- src/main/java/com/pouppe/server/PouppeApplication.java
+++ src/main/java/com/pouppe/server/PouppeApplication.java
@@ -9,5 +9,4 @@ public class PouppeApplication {
     public static void main(String[] args) {
         SpringApplication.run(PouppeApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/pouppe/server/configuration/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.pouppe.server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        sessionConfigurer -> sessionConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+
+        return http.build();
+    }
+}
