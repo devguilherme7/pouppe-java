4c79a15ac710e6545540f82fb64d96a45460363a
Guilherme Oliveira <dev.guilherme7@gmail.com>
2025-04-03 01:07:37 -0300

    Initial Commit

---
+++ src/test/java/com/pouppe/server/PouppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pouppe.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PouppeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
